6fa97f0ee59a21448aaa1e46153f601c38d81af5
minhmap2302 <nguyentienminh.230206@gmail.com>
2024-11-27 13:31:40 +0700

    baii 4 ss6.cpp

--- baii 4 ss6.cpp
+++ baii 4 ss6.cpp
@@ -20,22 +20,22 @@ int main() {
             printf("nghiem cua pt %dx^2 + %dx + %d = 0 la %f\n",a ,b ,c ,x1 );
         }else{
             if(c==0){
-                printf("phuong trÏnh vÙ s? nghi?m");
+                printf("phuong tr√¨nh v√¥ s? nghi?m");
             }else{
-                printf("phuong trÏnh vÙ nghi?m");
+                printf("phuong tr√¨nh v√¥ nghi?m");
             }
         }
     }else if(a!=0){
         dt = b*b-4*a*c;
         if(dt==0){
             x1=-b/2*a;
-            printf("phuong trÏnh cÛ 1 nghi?m kÈp l‡ %f\n",x1);
+            printf("phuong tr√¨nh c√≥ 1 nghiem kep l√† %f\n",x1);
         }else if(dt >0){
             x1=(-b+sqrt(dt))/2*a;
             x2=(-b-sqrt(dt))/2*a;
-            printf("phuong trÏnh cÛ 2 nghi?m x1 = %f v‡ x2 = %f\n", x1 ,x2);
+            printf("phuong tr√¨nh c√≥ 2 nghiem x1 = %f v√† x2 = %f\n", x1 ,x2);
         }else if (dt<0){
-            printf("phuong trÏnh vÙ nghi?m");
+            printf("phuong tr√¨nh v√¥ nghiem");
         }
     }
 
